49795d7271114473454aafe3c04dca04005dce69
sujin <sujin@218.38.137.28>
2019-09-11 15:19:46 +0900

    init lotto

---
+++ lotto/src/main/java/net/bestmember/isjung/lotto/MegaMillion.java
@@ -0,0 +1,47 @@
+package net.bestmember.isjung.lotto;
+
+import java.util.Scanner;
+
+public class MegaMillion {
+	public static void main(String[] args) {
+		Scanner inp = new Scanner(System.in);
+		final int MAX = 70;
+		int[] set = new int[MAX]; // Holds the set of numbers
+		int cnt, // General purpose loop counter
+				elem; // What is the range of numbers?
+		int temp; // Random number generated
+		String response; // Another set of numbers?
+
+		do {
+			elem = MAX;
+			System.out.print("\nNumbers:  ");
+
+			// Fill the array with the possible numbers.
+			for (cnt = 0; cnt < MAX; cnt++)
+				set[cnt] = cnt + 1;
+
+			// Pick 5 numbers.
+			for (cnt = 0; cnt < 5; cnt++) {
+				// Random number points to an element of the array.
+				temp = (int) (Math.random() * elem);
+
+				// Add the number picked to the output string
+				System.out.print(set[temp] + "  ");
+
+				// "Close down" the array to remove the number selected.
+				for (int i = temp; i < elem - 1; i++)
+					set[i] = set[i + 1];
+
+				// Decrease the maximum number
+				elem--;
+			}
+
+			int powerball = (int) (Math.random() * 25) + 1;
+			System.out.println(" MegaMillion: " + powerball);
+
+			System.out.print("\nEnter \"y\" for another set of numbers," + "\"n\" to quit: ");
+			response = inp.next();
+			response = response.toLowerCase();
+		} while (response.equals("y"));
+	}
+}
