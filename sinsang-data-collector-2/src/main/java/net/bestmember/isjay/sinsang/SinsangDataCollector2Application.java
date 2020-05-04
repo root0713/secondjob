f11d0351da43f5aa3ca9ec9b28f9294b97c73354
isjung <isjung@localhost>
2020-05-04 21:25:45 +0900

    add scheduler

--- sinsang-data-collector-2/src/main/java/net/bestmember/isjay/sinsang/SinsangDataCollector2Application.java
+++ sinsang-data-collector-2/src/main/java/net/bestmember/isjay/sinsang/SinsangDataCollector2Application.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import net.bestmember.isjay.sinsang.mapper.ProductImageMapper;
 import net.bestmember.isjay.sinsang.mapper.ProductMapper;
 
 @SpringBootApplication
+@EnableScheduling
 public class SinsangDataCollector2Application {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     
